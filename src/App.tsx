9f69098956edf9a28d4baf5526e57e1b9fed8120
Carlos Ucan <carlos.ucan@chapur.com>
2023-11-08 14:34:06 -0600

    Flowbite implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './sections/NavBar';
+
+import Principal from './pages/Principal';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={ <Principal /> } />
+          <Route path='/home' element={ <Home /> } />
+          <Route path='/services' element={ <Services /> } />
+          <Route path='/contact' element={ <Contact /> } />
+          <Route path='/log-out' element={ <Navigate replace to="/" /> } />
+          <Route path='*' element={ <NotFound /> } />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
